8eb471716a373e4f6dddc47f230427298a332e33
Mukesh16407 <mukesh.16407@gmail.com>
2023-12-08 16:34:36 +0530

    authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticalsModule } from './articals/articals.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, ArticalsModule, UsersModule],
+  imports: [PrismaModule, ArticalsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
